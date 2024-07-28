9f86681991cc98984361dd217b15c8dd7c1c2998
Muhammad Bilal Khan <mynamebilalkhan@gmail.com>
2024-07-28 16:36:48 +0500

    Contact, Not Found Page

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import {
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<NotFound />} />
     </Route>
   )
 );
