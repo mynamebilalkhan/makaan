d935dc1227ff644054c3d575663533ee51c1b46d
BilalK <test.dev427@gmail.com>
2024-07-30 23:10:21 +0500

    Property Search Functionality

--- src/components/PropertySearch.jsx
+++ src/components/PropertySearch.jsx
@@ -1,4 +1,40 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const PropertySearch = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [category, setCategory] = useState("");
+  const [location, setLocation] = useState("");
+  const navigate = useNavigate();
+
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleTypeChange = (event) => {
+    setCategory(event.target.value);
+  };
+
+  const handleLocationChange = (event) => {
+    setLocation(event.target.value);
+  };
+
+  const navigateResults = () => {
+    const queryParams = new URLSearchParams({
+      search: searchQuery,
+      category: category,
+      location: location,
+    }).toString();
+
+    navigate(`/listing?${queryParams}`);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // console.log(inputs);
+    navigateResults();
+  };
+
   return (
     <>
       <div
@@ -7,40 +43,54 @@ const PropertySearch = () => {
         style={{ padding: "35px" }}
       >
         <div className="container">
-          <div className="row g-2">
-            <div className="col-md-10">
-              <div className="row g-2">
-                <div className="col-md-4">
-                  <input
-                    type="text"
-                    className="form-control border-0 py-3"
-                    placeholder="Search Keyword"
-                  />
-                </div>
-                <div className="col-md-4">
-                  <select className="form-select border-0 py-3" defaultValue="">
-                    <option value="">Property Type</option>
-                    <option value="1">Property Type 1</option>
-                    <option value="2">Property Type 2</option>
-                    <option value="3">Property Type 3</option>
-                  </select>
-                </div>
-                <div className="col-md-4">
-                  <select className="form-select border-0 py-3" defaultValue="">
-                    <option value="">Location</option>
-                    <option value="1">Location 1</option>
-                    <option value="2">Location 2</option>
-                    <option value="3">Location 3</option>
-                  </select>
+          <form onSubmit={handleSubmit}>
+            <div className="row g-2">
+              <div className="col-md-10">
+                <div className="row g-2">
+                  <div className="col-md-4">
+                    <input
+                      type="text"
+                      className="form-control border-0 py-3"
+                      placeholder="Search Keyword"
+                      name="keyword"
+                      onChange={handleSearchChange}
+                    />
+                  </div>
+                  <div className="col-md-4">
+                    <select
+                      className="form-select border-0 py-3"
+                      defaultValue=""
+                      name="type"
+                      onChange={handleTypeChange}
+                    >
+                      <option value="">Property Type</option>
+                      <option value="1">Property Type 1</option>
+                      <option value="2">Property Type 2</option>
+                      <option value="3">Property Type 3</option>
+                    </select>
+                  </div>
+                  <div className="col-md-4">
+                    <select
+                      className="form-select border-0 py-3"
+                      defaultValue=""
+                      name="location"
+                      onChange={handleLocationChange}
+                    >
+                      <option value="">Location</option>
+                      <option value="1">Location 1</option>
+                      <option value="2">Location 2</option>
+                      <option value="3">Location 3</option>
+                    </select>
+                  </div>
                 </div>
               </div>
+              <div className="col-md-2">
+                <button className="btn btn-dark border-0 w-100 py-3">
+                  Search
+                </button>
+              </div>
             </div>
-            <div className="col-md-2">
-              <button className="btn btn-dark border-0 w-100 py-3">
-                Search
-              </button>
-            </div>
-          </div>
+          </form>
         </div>
       </div>
     </>
